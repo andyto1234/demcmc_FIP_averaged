91277a7d49316d77d774dc0e8dbf1413b8f996b8
andyto1234 <toshuho@gmail.com>
2024-08-05 16:52:56 +0200

    first commit

---
+++ eis_average/function.py
@@ -0,0 +1,78 @@
+import numpy as np
+from astropy import units as u
+from sunpy.coordinates import frames
+from ndcube import NDCube
+from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord, SpectralCoord
+from ndcube import NDCube
+from eispac.core.eiscube import EISCube
+
+
+def average_spectral_data(data_cube, tmplt, lower_left, upper_right, shift2fexii = True, ref_wave=195.12):
+
+    from scipy.ndimage import shift as shift_img
+    from eispac.instr.ccd_offset import ccd_offset
+    from astropy.wcs.utils import wcs_to_celestial_frame
+
+    """Average spectral data within a specified rectangular region.
+
+    Parameters:
+    - data_cube (ndcube.NDCube): The input data cube containing spectral data.
+    - template (dict): A dictionary representing the template with 'data_x' key containing wavelength values.
+    - lower_left (list): The lower-left corner coordinates of the rectangular region [x, y].
+    - upper_right (list): The upper-right corner coordinates of the rectangular region [x, y].
+
+    Returns:
+    - data_cutout_sum (ndcube.NDCube): The averaged spectral data within the specified region,
+      rebinned into a single macropixel.
+    """
+
+    eis_frame = wcs_to_celestial_frame(data_cube.wcs)
+    print(eis_frame)
+    
+    if shift2fexii:
+        this_wave = np.mean(tmplt.template['data_x'])
+        disp = ccd_offset(ref_wave) - ccd_offset(this_wave)
+        print(f'SHIFT2WAVE: shifted to {ref_wave} FOV according to CCD offset - OFFSET: {disp[0]:.1f}')
+    else:
+        disp = 0
+    
+    print(f"old bottom ty: {lower_left[1]}")
+    print(f"new bottom ty: {lower_left[1]-disp[0]}")
+    lower_left = [SpectralCoord(min(tmplt.template['data_x']), unit=u.angstrom),
+                  SkyCoord(Tx=lower_left[0], Ty=int(lower_left[1]-disp[0]), unit=u.arcsec, frame=eis_frame)]
+    upper_right = [SpectralCoord(max(tmplt.template['data_x']), unit=u.AA),
+                   SkyCoord(Tx=upper_right[0], Ty=int(upper_right[1]-disp[0]), unit=u.arcsec, frame=eis_frame)]
+    
+
+    # Crop ndcube data to the desired subpixels
+    data_cutout = data_cube.crop(lower_left, upper_right)
+    
+    # Calculate the mean wavelength
+    wavelength_average = np.nanmean(data_cutout.wavelength, axis=(0, 1))
+
+    # Calculate the averaged error
+    sum_squared_errors = np.nansum(data_cutout.uncertainty.array**2, axis=(0, 1))
+    
+    # Calculate the averaged error by taking the square root of the sum of squared errors divided by the number of elements
+    num_elements = data_cutout.data.shape[0] * data_cutout.data.shape[1]
+    averaged_error = np.sqrt(sum_squared_errors / num_elements)
+
+    # Rebin data into 1 single pixel - default is np.mean
+    hack_rebinned = NDCube(data_cutout.data, wcs=data_cutout.wcs).rebin(np.array([data_cutout.data[:,0,0].shape[0],data_cutout.data[0,:,0].shape[0],1]))
+    data_cutout_sum = EISCube(hack_rebinned, wcs = hack_rebinned.wcs, 
+                              uncertainty = [[averaged_error]],
+                              wavelength = np.array([[wavelength_average]]),
+                              unit = 'erg / (cm2 s sr)',
+                              meta = data_cutout.meta)
+    return data_cutout_sum, data_cutout
+
+
+    # data_cutout_sum = data_cutout.rebin(np.array([data_cutout.data[:,0,0].shape[0],data_cutout.data[0,:,0].shape[0],1]))
+    # # data_cutout_sum = EISCube(hack_cube.rebin(np.array([data_cutout.data[:,0,0].shape[0],data_cutout.data[0,:,0].shape[0],1])), unit = data_cutout.unit, meta = data_cutout.meta)
+
+    # # # fit everything into the final 1 macropixel datacube
+    # data_cutout_sum.uncertainty = [[averaged_error]]
+    # data_cutout_sum.wavelength = np.array([[wavelength_average]])
+
+    # return data_cutout_sum, data_cutout
---
+++ eis_average_pixel.py
@@ -0,0 +1,149 @@
+import numpy as np
+import astropy.units as u
+import eispac
+from eis_average.function import average_spectral_data
+from eis_calibration.eis_calib_2014 import calib_2014
+from ashmcmc import ashmcmc, interp_emis_temp
+from demcmc import EmissionLine, TempBins, ContFuncDiscrete
+from mcmc.mcmc_utils import calc_chi2, mcmc_process
+from mcmc_para import pred_intensity_compact
+import glob
+from asheis import asheis
+import pdb  # Add this import at the top of your file
+
+"""
+This script processes EIS data to perform spectral analysis and 
+differential emission measure (DEM) calculations. It includes the following main steps:
+
+1. Process individual emission lines:
+    - Reads EIS data and fit templates
+    - Averages spectral data within a specified region
+    - Fits the spectra and applies calibration
+
+2. Calculate electron density using Fe XIII line pair
+
+3. Prepare data for MCMC (Markov Chain Monte Carlo) analysis:
+    - Reads emissivity data
+    - Creates emission line objects for MCMC
+
+4. Perform MCMC process to derive DEM
+
+5. Calculate FIP (First Ionization Potential) ratios
+
+The script utilizes various modules including eispac for EIS data handling, 
+ashmcmc for MCMC calculations, and custom modules for specific functionalities.
+
+Usage:
+This script is designed to be run as part of a larger analysis pipeline. 
+It requires pre-defined input parameters such as EIS event file path and 
+coordinates for the region of interest.
+
+Note: Some parts of the code (e.g., FIP ratio calculation) are prepared but 
+not fully implemented in the main execution flow.
+"""
+
+def process_line(line, dict, eis_evt, r1_bl_crd, r1_tr_crd):
+    template_name=dict[f'{line}'][0]
+    if template_name != 'fe_13_203_826.2c.template.h5':
+        template = eispac.read_template(eispac.data.get_fit_template_filepath(template_name))
+    else:
+        template = eispac.read_template('eis_density/fe_13_203_830.3c.template.h5')
+        template_name = 'fe_13_203_830.3c.template.h5'
+
+
+    data_cube = eispac.read_cube(eis_evt, template.central_wave)
+    data_averaged, data_cropped = average_spectral_data(
+        data_cube, template, 
+        [r1_bl_crd[0], r1_bl_crd[1]], 
+        [r1_tr_crd[0], r1_tr_crd[1]]
+    )
+    # pdb.set_trace()
+
+    fit_res = eispac.fit_spectra(data_averaged, template, ncpu='max')
+    fitted_result = fit_res.get_map(component=dict[f'{line}'][1], measurement='intensity')
+    fitted_result, ratio = calib_2014(fitted_result, ratio=True)
+    # Print the calibration ratio prettily
+    print(f'---------------------Calibrated using Warren et al. 2014; Ratio: {ratio:.2f}---------------------')
+    return fitted_result.data[0][0]
+
+
+def eis_averaged_fip(eis_evt, r1_bl_crd, r1_tr_crd):
+    # Initiate asheis object
+    eis_cube = asheis(eis_evt)
+    ash = ashmcmc(eis_evt)
+    Lines, dim, dem_num = ash.check_existing_lines()
+    dict = eis_cube.dict
+
+    # Process emission lines
+    Intensity = np.array([process_line(line, dict, eis_evt, r1_bl_crd, r1_tr_crd) for line in Lines])
+    # pdb.set_trace()
+
+    # Process density lines
+    density_lines = ['fe_13_203.83', 'fe_13_202.04']
+    density_intensity = np.array([process_line(line, dict, eis_evt, r1_bl_crd, r1_tr_crd) for line in density_lines])
+    dens_ratio = density_intensity[0] / density_intensity[1]
+    density_ratios, density_values = eis_cube.read_density_file('fe_13_203.83')
+    ldens = eis_cube.find_closest_density(dens_ratio, density_ratios, density_values)
+
+    # Prepare MCMC
+    logt, emis, linenames = ash.read_emissivity(ldens)
+    logt_interp = interp_emis_temp(logt.value)
+    temp_bins = TempBins(logt_interp * u.K)
+    emis_sorted = ash.emis_filter(emis, linenames, Lines)
+
+    # Create MCMC lines
+    mcmc_lines = []
+    for ind, line in enumerate(Lines):
+        if line.startswith('fe') and Intensity[ind] > 10:
+            mcmc_emis = ContFuncDiscrete(
+                logt_interp * u.K,
+                interp_emis_temp(emis_sorted[ind, :]) * u.cm ** 5 / u.K,
+                name=line
+            )
+            mcmc_intensity = Intensity[ind]
+            mcmc_int_error = Intensity[ind] * 0.3
+            emissionLine = EmissionLine(
+                mcmc_emis,
+                intensity_obs=mcmc_intensity,
+                sigma_intensity_obs=mcmc_int_error,
+                name=line
+            )
+            mcmc_lines.append(emissionLine)
+
+    # Process MCMC
+    # Print a pretty message for DEM calculation using MCMC
+    print("Calculating DEM using MCMC".center(50))
+    dem_median = mcmc_process(mcmc_lines, temp_bins)
+    chi2 = calc_chi2(mcmc_lines, dem_median, temp_bins)
+
+    print(mcmc_intensity)
+    # Process FIP ratios
+    line_databases = {
+        "sis": ['si_10_258.37', 's_10_264.23', 'SiX_SX'],
+        # "CaAr": ['ca_14_193.87', 'ar_14_194.40', 'CaXIV_ArXIV'],
+    }
+
+    for comp_ratio, lines in line_databases.items():
+        intensities = np.array([process_line(line, dict, eis_evt, r1_bl_crd, r1_tr_crd) for line in lines[:2]])
+        
+        logt, emis, linenames = ash.read_emissivity(ldens)
+        logt_interp = interp_emis_temp(logt.value)
+        emis_sorted = ash.emis_filter(emis, linenames, lines[:2])
+        
+        int_lf = pred_intensity_compact(emis_sorted[0], logt_interp, lines[0], dem_median)
+        dem_scaled = dem_median * (intensities[0] / int_lf)
+        int_hf = pred_intensity_compact(emis_sorted[1], logt_interp, lines[1], dem_scaled)
+        fip_ratio = int_hf / intensities[1]
+
+        print(f"FIP ratio for {comp_ratio}: {fip_ratio}")
+        print(f"Number of lines used for DEM: {len(mcmc_lines)}")
+        print(f"Chi-squared value: {chi2}")
+
+    return fip_ratio, mcmc_lines, chi2
+
+if __name__ == "__main__":
+    eis_evts = sorted(glob.glob('/Users/andysh.to/Script/Python_Script/demcmc_FIP_averaged/data_eis/*.data.h5'))
+    eis_evt = eis_evts[0]
+    r1_bl_crd = [216.88862131045445, -210.08356892192884]
+    r1_tr_crd = [220.8914582334983, -206.08513765573548]
+    fip_ratio, mcmc_lines, chi2 = eis_averaged_fip(eis_evt, r1_bl_crd, r1_tr_crd)
